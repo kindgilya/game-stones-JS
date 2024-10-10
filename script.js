2a229d4537121c97c7f939d62740f81d1337196e
Elena <ed41658@gmail.com>
2024-10-10 10:45:15 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const root = document.querySelector(".root");
+
+function createElement(html) {
+    const div = document.createElement("div");
+    div.insertAdjacentHTML("beforeend", html);
+    return div.firstElementChild;
+  }
+
+
+class Game {
+    _element = null;
+    _sub
+    constructor(){
+
+    }
+}
