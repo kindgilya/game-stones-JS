d79fb4f5bf564be7bc3c0b058f5e0c5ce784ab1e
Elena <ed41658@gmail.com>
2024-10-10 11:02:56 +0500

    create template for Stone class

--- script.js
+++ script.js
@@ -29,13 +29,52 @@ class Game {
         }, {});
       }
 
-    _getTemplate(){
+			_getTemplate(){
         return `<div class="game">
 				<div class="game__btn"></div>
                 <span class="game__found">Found parts: 0</span>
                 <span class="game__total">Total parts: 0</span>
                 <div class="game__field"></div>
         </div>`
+    }
+
+    get element() {
+        return this._element;
+    }
+}
+
+
+class Stone {
+    _element = null;
+    _subElements = null;
+		
+    constructor({id, disabled, hide, pair, img}){
+        this._id = id;
+        this._disabled = disabled;
+        this._hide = hide;
+        this._pair = pair;
+        this._img = img;
+        this._init();
+    }
+
+    _init(){
+        this._element = createElement(this._getTemplate());
+        this._subElements = this._getSubElements();
+    }
+
+    _getSubElements() {
+        return Array.from(this._element.querySelectorAll("[data-element]")).reduce((acc, elem) => {
+          return {
+            ...acc,
+            [elem.getAttribute("data-element")]: elem,
+          };
+        }, {});
+      }
+
+			_getTemplate(){
+        return `<button class="stone ${this._hide ? "stone--hide" : ""} ${this._pair ? "stone--pair" : ""}" ${this._disabled ? "disabled" : ""}>
+                <img class="stone__img" src="/img/${this._img}" alt="">
+                </button>`
     }
 
     get element() {
