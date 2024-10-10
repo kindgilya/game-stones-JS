58ab69370276ec64fad773532dac9dda6a611740
Elena <ed41658@gmail.com>
2024-10-10 10:54:26 +0500

    Create base template Game component

--- script.js
+++ script.js
@@ -9,8 +9,36 @@ function createElement(html) {
 
 class Game {
     _element = null;
-    _sub
+    _subElements = null;
+		
     constructor(){
+        this._init();
+    }
 
+    _init(){
+        this._element = createElement(this._getTemplate());
+        this._subElements = this._getSubElements();
+    }
+
+    _getSubElements() {
+        return Array.from(this._element.querySelectorAll("[data-element]")).reduce((acc, elem) => {
+          return {
+            ...acc,
+            [elem.getAttribute("data-element")]: elem,
+          };
+        }, {});
+      }
+
+    _getTemplate(){
+        return `<div class="game">
+				<div class="game__btn"></div>
+                <span class="game__found">Found parts: 0</span>
+                <span class="game__total">Total parts: 0</span>
+                <div class="game__field"></div>
+        </div>`
+    }
+
+    get element() {
+        return this._element;
     }
 }
