1b2a6d2d90e44c27d006a8a0b5b4271e7a1dad28
Elena <ed41658@gmail.com>
2024-10-10 11:11:14 +0500

    create Button class template

--- script.js
+++ script.js
@@ -63,19 +63,49 @@ class Stone {
     }
 
     _getSubElements() {
-        return Array.from(this._element.querySelectorAll("[data-element]")).reduce((acc, elem) => {
-          return {
-            ...acc,
-            [elem.getAttribute("data-element")]: elem,
-          };
-        }, {});
-      }
+      return Array.from(this._element.querySelectorAll("[data-element]")).reduce((acc, elem) => {
+        return {
+          ...acc,
+          [elem.getAttribute("data-element")]: elem,
+        };
+      }, {});
+    }
+  
 
-			_getTemplate(){
+	_getTemplate(){
         return `<button class="stone ${this._hide ? "stone--hide" : ""} ${this._pair ? "stone--pair" : ""}" ${this._disabled ? "disabled" : ""}>
                 <img class="stone__img" src="/img/${this._img}" alt="">
                 </button>`
     }
+
+    get element() {
+        return this._element;
+    }
+}
+
+class Button {
+    _element = null;
+		
+    constructor({use, disabled, text}, handler){
+        this._use = use;
+        this._disabled = disabled;
+        this._text = text;
+        this._handler = handler;
+        this._init();
+    }
+
+    _init(){
+        this._element = createElement(this._getTemplate());
+        this._addListeners();
+    }
+
+	_getTemplate(){
+        return `<button class="btn btn--${this._use}" ${this._disabled ? "disabled" : ""}>${text}</button>`
+    }
+
+	_addListeners() {
+       this._element.addEventListener("click", () => this._handler()) 
+	}
 
     get element() {
         return this._element;
